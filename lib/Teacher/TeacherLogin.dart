53fccfd531469f7f76f43d35d68668c63d6eed15
Gajjar Mohit <debugntitylab.io@gmail.com>
2021-09-03 11:45:09 +0530

    Implimented Teacher Login

--- lib/Teacher/TeacherLogin.dart
+++ lib/Teacher/TeacherLogin.dart
@@ -1,5 +1,7 @@
+import 'package:asms/Authentication/Auth_service.dart';
 import 'package:asms/Constants/Constants.dart';
 import 'package:asms/Constants/ForgotPassword.dart';
+import 'package:asms/Database/DatabaseMethods.dart';
 import 'package:asms/Teacher/TeacherHome.dart';
 import 'package:asms/Constants/Widgets.dart';
 import 'package:flutter/material.dart';
@@ -12,54 +14,79 @@ class TeacherLogin extends StatefulWidget {
 }
 
 class _TeacherLoginState extends State<TeacherLogin> {
+  final formKey = GlobalKey<FormState>();
   TextEditingController emailController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
-  TextEditingController usernameController = new TextEditingController();
+ 
+  signIn() async {
+    DatabaseMethods().getTeacherBy(emailController.text);
+    AuthMethod()
+        .signInWithEmailAndPassword(
+            emailController.text, passwordController.text)
+        .then((value) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => TeacherHome()));
+      print("Teacher Login is working");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backColor,
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                imageBox('assets/1.png', 350),
+                text("Teacher Login", 30),
+                SizedBox(
+                  height: 20,
+                ),
+                inputFieldemail("Enter Email", Icons.person, context,
+                    emailController),
+                SizedBox(
+                  height: 20,
+                ),
+                inputFieldPass("Enter Password", Icons.vpn_key, context,
+                    passwordController),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                          onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ForgotPass())),
+                          child: text_underline("Forgot Password?", 16)),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      if (emailController.text.isNotEmpty &&
+                          passwordController.text.isNotEmpty) {
+                        signIn();
+                        print("Teacher Sigin working");
+                      }
+                    },
+                    child: btn("Login", 100))
+              ],
             ),
-            imageBox('assets/1.png', 350),
-            text("Teacher Login", 30),
-            SizedBox(
-              height: 20,
-            ),
-            inputFieldUsername("Enter Username", Icons.person, context,usernameController),
-            SizedBox(
-              height: 20,
-            ),
-            inputFieldPass("Enter Password", Icons.vpn_key, context,passwordController),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width - 60,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  GestureDetector(
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ForgotPass())),
-                      child: text_underline("Forgot Password?", 16)),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-                onTap: () => Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => TeacherHome())),
-                child: btn("Login", 100))
-          ],
+          ),
         ),
       ),
     );
